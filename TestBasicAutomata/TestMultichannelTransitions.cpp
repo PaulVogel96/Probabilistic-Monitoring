1902ea0bd0b82c4486a877aa025137ef91443a7f
PaulVogel96 <vogel.paul1996@gmail.com>
2025-11-06 12:13:04 +0100

    Unit Tests for new transitions

--- TestBasicAutomata/TestMultichannelTransitions.cpp
+++ TestBasicAutomata/TestMultichannelTransitions.cpp
@@ -16,23 +16,197 @@ class TestMultichannelTransitions : public TestOnce {
   }
 };
 
-// //Tests that processEvents() behaves identical to multiple changeStates() calls
-// testF(TestMultichannelTransitions, TestProcessEventsEqualsMultipleChangeStates) {
-//   //given
-//   PActiveTransition transition = new PActiveTransition()
+testF(TestMultichannelTransitions, AllRequiredEventsActiveTransition_fires_when_all_required_events_are_passed_single_event) {
+  //given
+  State<ProbTransition> a("A", Verdict::INCONCLUSIVE);
+  State<ProbTransition> b("B", Verdict::SATISFIED);
+  AllRequiredEventsActiveTransition t1(&a, &b, 1.0, EVENT_P);
 
-//   //when
-//   automaton1.changeStates('a');
-//   automaton1.changeStates('a');
-//   automaton1.changeStates('a');
-//   automaton2.processEvents("aaa");
-//   std::map<String, float> results1 = automaton1.getStateProbabilities();
-//   std::map<String, float> results2 = automaton2.getStateProbabilities();
+  //when
+  bool result = t1.shouldFire(EVENT_P);
 
-//   //then
-//   assertTrue(results1.size() == results2.size());
-//   assertTrue(approxEqual(results1["A"], results2["A"]));
-//   assertTrue(approxEqual(results1["A"], 0.125));
-//   assertTrue(approxEqual(results1["B"], results2["B"]));
-//   assertTrue(approxEqual(results1["B"], 0.875));
-// }
+  //then
+  assertTrue(result);
+}
+
+testF(TestMultichannelTransitions, AllRequiredEventsActiveTransition_does_not_fire_when_not_all_required_events_are_passed_single_event) {
+  //given
+  State<ProbTransition> a("A", Verdict::INCONCLUSIVE);
+  State<ProbTransition> b("B", Verdict::SATISFIED);
+  AllRequiredEventsActiveTransition t1(&a, &b, 1.0, EVENT_Q);
+
+  //when
+  bool result = t1.shouldFire(EVENT_P);
+
+  //then
+  assertFalse(result);
+}
+
+testF(TestMultichannelTransitions, AllRequiredEventsActiveTransition_does_fire_when_all_required_events_are_passed_multiple_events) {
+  //given
+  State<ProbTransition> a("A", Verdict::INCONCLUSIVE);
+  State<ProbTransition> b("B", Verdict::SATISFIED);
+  AllRequiredEventsActiveTransition t1(&a, &b, 1.0, EVENT_P | EVENT_Q);
+
+  //when
+  bool result = t1.shouldFire(EVENT_P | EVENT_Q);
+
+  //then
+  assertTrue(result);
+}
+
+testF(TestMultichannelTransitions, AllRequiredEventsActiveTransition_does_not_fire_when_not_all_required_events_are_passed_multiple_events) {
+  //given
+  State<ProbTransition> a("A", Verdict::INCONCLUSIVE);
+  State<ProbTransition> b("B", Verdict::SATISFIED);
+  AllRequiredEventsActiveTransition t1(&a, &b, 1.0, EVENT_P | EVENT_Q);
+
+  //when
+  bool result = t1.shouldFire(EVENT_P);
+
+  //then
+  assertFalse(result);
+}
+
+testF(TestMultichannelTransitions, AllRequiredEventsActiveTransition_additional_properties_do_not_matter) {
+  //given
+  State<ProbTransition> a("A", Verdict::INCONCLUSIVE);
+  State<ProbTransition> b("B", Verdict::SATISFIED);
+  AllRequiredEventsActiveTransition t1(&a, &b, 1.0, EVENT_P);
+
+  //when
+  bool result = t1.shouldFire(EVENT_P | EVENT_Q | EVENT_R | EVENT_S);
+
+  //then
+  assertTrue(result);
+}
+
+testF(TestMultichannelTransitions, AnyRequiredEventsActiveTransition_fires_when_any_required_events_are_passed_single_event) {
+  //given
+  State<ProbTransition> a("A", Verdict::INCONCLUSIVE);
+  State<ProbTransition> b("B", Verdict::SATISFIED);
+  AnyRequiredEventsActiveTransition t1(&a, &b, 1.0, EVENT_P);
+
+  //when
+  bool result = t1.shouldFire(EVENT_P);
+
+  //then
+  assertTrue(result);
+}
+
+testF(TestMultichannelTransitions, AnyRequiredEventsActiveTransition_does_not_fire_when_none_of_required_events_are_passed_single_event) {
+  //given
+  State<ProbTransition> a("A", Verdict::INCONCLUSIVE);
+  State<ProbTransition> b("B", Verdict::SATISFIED);
+  AnyRequiredEventsActiveTransition t1(&a, &b, 1.0, EVENT_Q);
+
+  //when
+  bool result = t1.shouldFire(EVENT_P);
+
+  //then
+  assertFalse(result);
+}
+
+testF(TestMultichannelTransitions, AnyRequiredEventsActiveTransition_does_fire_when_any_required_events_are_passed_multiple_events_possible) {
+  //given
+  State<ProbTransition> a("A", Verdict::INCONCLUSIVE);
+  State<ProbTransition> b("B", Verdict::SATISFIED);
+  AnyRequiredEventsActiveTransition t1(&a, &b, 1.0, EVENT_P | EVENT_Q);
+
+  //when
+  bool result = t1.shouldFire(EVENT_P);
+
+  //then
+  assertTrue(result);
+}
+
+testF(TestMultichannelTransitions, AnyRequiredEventsActiveTransition_does_not_fire_when_none_of_the_required_events_are_passed_multiple_events) {
+  //given
+  State<ProbTransition> a("A", Verdict::INCONCLUSIVE);
+  State<ProbTransition> b("B", Verdict::SATISFIED);
+  AnyRequiredEventsActiveTransition t1(&a, &b, 1.0, EVENT_P | EVENT_Q);
+
+  //when
+  bool result = t1.shouldFire(EVENT_S);
+
+  //then
+  assertFalse(result);
+}
+
+testF(TestMultichannelTransitions, AnyRequiredEventsActiveTransition_additional_properties_do_not_matter) {
+  //given
+  State<ProbTransition> a("A", Verdict::INCONCLUSIVE);
+  State<ProbTransition> b("B", Verdict::SATISFIED);
+  AnyRequiredEventsActiveTransition t1(&a, &b, 1.0, EVENT_P);
+
+  //when
+  bool result = t1.shouldFire(EVENT_P | EVENT_Q | EVENT_R | EVENT_S);
+
+  //then
+  assertTrue(result);
+}
+
+testF(TestMultichannelTransitions, ExactEventsMatchTransition_fires_when_required_events_are_passed_single_event) {
+  //given
+  State<ProbTransition> a("A", Verdict::INCONCLUSIVE);
+  State<ProbTransition> b("B", Verdict::SATISFIED);
+  ExactEventsMatchTransition t1(&a, &b, 1.0, EVENT_Q);
+
+  //when
+  bool result = t1.shouldFire(EVENT_Q);
+
+  //then
+  assertTrue(result);
+}
+
+testF(TestMultichannelTransitions, ExactEventsMatchTransition_does_not_fire_when_none_of_required_events_are_passed_single_event) {
+  //given
+  State<ProbTransition> a("A", Verdict::INCONCLUSIVE);
+  State<ProbTransition> b("B", Verdict::SATISFIED);
+  ExactEventsMatchTransition t1(&a, &b, 1.0, EVENT_Q);
+
+  //when
+  bool result = t1.shouldFire(EVENT_P);
+
+  //then
+  assertFalse(result);
+}
+
+testF(TestMultichannelTransitions, ExactEventsMatchTransition_does_fire_when_exact_required_events_are_passed_multiple_events_possible) {
+  //given
+  State<ProbTransition> a("A", Verdict::INCONCLUSIVE);
+  State<ProbTransition> b("B", Verdict::SATISFIED);
+  ExactEventsMatchTransition t1(&a, &b, 1.0, EVENT_P | EVENT_Q);
+
+  //when
+  bool result = t1.shouldFire(EVENT_P | EVENT_Q);
+
+  //then
+  assertTrue(result);
+}
+
+testF(TestMultichannelTransitions, ExactEventsMatchTransition_does_not_fire_when_none_of_the_required_events_are_passed_multiple_events) {
+  //given
+  State<ProbTransition> a("A", Verdict::INCONCLUSIVE);
+  State<ProbTransition> b("B", Verdict::SATISFIED);
+  ExactEventsMatchTransition t1(&a, &b, 1.0, EVENT_P | EVENT_Q);
+
+  //when
+  bool result = t1.shouldFire(EVENT_S);
+
+  //then
+  assertFalse(result);
+}
+
+testF(TestMultichannelTransitions, ExactEventsMatchTransition_properties_leads_to_not_firing) {
+  //given
+  State<ProbTransition> a("A", Verdict::INCONCLUSIVE);
+  State<ProbTransition> b("B", Verdict::SATISFIED);
+  ExactEventsMatchTransition t1(&a, &b, 1.0, EVENT_P);
+
+  //when
+  bool result = t1.shouldFire(EVENT_P | EVENT_Q | EVENT_R | EVENT_S);
+
+  //then
+  assertFalse(result);
+}
